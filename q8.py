718b25f5395cbd5d6ed58cc2be148aee7f6ddec4
XifeiNi <cecilia.ni@student.unsw.edu.au>
2019-11-15 00:02:40 +1100

    q8 with time clashes

--- q8.py
+++ q8.py
@@ -2,6 +2,13 @@
 import sys
 from collections import defaultdict
 import cs3311
+class timeslot(object):
+	def __init__(self, time, typed, course):
+		self.time = time
+		self.typed = typed
+		self.course = course
+	def __str__(self):
+		return self.course + ' ' + str(self.typed) + ' ' + str(self.time)
 def preprocess(course, ct, fetch):
 	course_time = defaultdict(list)
 	class_map = defaultdict(list)
@@ -10,6 +17,39 @@ def preprocess(course, ct, fetch):
 	for key in class_map:
 		course_time[key[1]].append(class_map[key])
 	ct[course] = course_time
+
+def types(ct):
+	ret = 0
+	for key in ct:
+		ret += len(ct[key])
+	return ret	
+def dumpCT(ct):
+	dump = defaultdict(list)
+	count = 0
+	for key in ct:
+		for typed in ct[key]:
+			for slot in ct[key][typed]:
+				dump[count].append(timeslot(slot, typed, key))
+			count += 1
+	return dump
+		
+def combinations(ct, types):
+	if not ct:
+		return []
+	results = []
+	dfs(types, 0, ct, [], results)
+	return results
+
+def dfs(num_type,index, ct, curr_com, results):
+	if index == num_type:
+		results.append(list(curr_com))
+		return
+
+	for time_slots in ct[index]:
+		curr_com.append(time_slots)
+		dfs(num_type, index + 1, ct, curr_com, results)
+		curr_com.pop()
+			
 
 conn = cs3311.connect()
 cur = conn.cursor()
@@ -24,8 +64,14 @@ ct = defaultdict(list)
 for course in courses:
 	cur.callproc('get_course_record', [course,])
 	preprocess(course, ct, cur.fetchall())
-print(ct)	
+#print(ct)	
+dump = dumpCT(ct)
 
-
+for com in combinations(dump, types(ct)):
+	string = ""
+	for slot in com:
+		string += str(slot)
+		string += ','
+	print(string)
 cur.close()
 conn.close()
